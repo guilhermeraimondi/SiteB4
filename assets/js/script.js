7be50a2d633badba9dce23838470379e99817522
Guilherme Raimondi <raimondi.it@gmail.com>
2018-05-09 19:07:06 -0300

    add preloader

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,156 @@
+function easeIn(t) {
+      return t * t * t;
+    }
+    function easeOut(t) {
+      return 1 - (1-t) * (1-t) * (1-t);
+    }
+    function easeInOut(t) {
+      if(t <= 0.5) {
+        return easeIn(t*2)/2;
+      }else{
+        return easeOut( (t - 0.5)*2 ) / 2 + 0.5;
+      }
+    }
+    (function(){
+      var SIZE = 60,
+        MARGIN = 20;
+
+      var canvas = document.getElementById("loader"),
+        ctx = canvas.getContext("2d");
+
+      var isGoingRight = true,
+        step = 0,
+        lastId = 1;
+
+      function Square(x, y) {
+        this.id = lastId++;
+        this.prevX = x;
+        this.prevY = y;
+        this.nextX = x;
+        this.nextY = y;
+        this.x = x;
+        this.y = y;
+        this.animation = 0;
+
+        Square.squares.push(this);
+      }
+      Square.squares = [];
+      Square.prototype.animate = function() {
+        this.animation = Math.min(this.animation + 0.05, 1);
+        var t = easeInOut(this.animation);
+
+        this.x = this.prevX + (this.nextX - this.prevX) * t;
+        this.y = this.prevY + (this.nextY - this.prevY) * t;
+
+        return this;
+      };
+      Square.prototype.draw = function(_ctx) {
+        var x = (canvas.width / 2) + (MARGIN / 2) +(SIZE*this.x) - (SIZE*2);
+        var y = (canvas.height / 2) + (MARGIN / 2) + (SIZE*this.y) - (SIZE*2);
+        _ctx.clearRect(x, y, SIZE - (MARGIN / 2), SIZE - (MARGIN / 2));
+      };
+
+      new Square(0, 1);
+      new Square(1, 1);
+      new Square(2, 1);
+      new Square(3, 1);
+
+      new Square(0, 0);
+      function loop() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        ctx.fillStyle = "#232323";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        for(var i = 0; i < Square.squares.length; i++)
+          Square.squares[i].animate().draw(ctx);
+
+        requestAnimationFrame(loop);
+      }
+      requestAnimationFrame(loop);
+
+      setInterval(function(){
+        for(var i = 0; i < Square.squares.length; i++){
+          var target = Square.squares[i];
+          target.animation = 0;
+          target.x = target.nextX;
+          target.y = target.nextY;
+
+          target.prevX = target.nextX;
+          target.prevY = target.nextY;
+        }
+        switch(step) {
+          case 0:
+            var target = null;
+
+            for(var i = 0; i < Square.squares.length; i++)
+              if(Square.squares[i].nextY == 0)
+                target = Square.squares[i];
+
+            target.nextY += 1;
+
+            for(var i = 0; i < Square.squares.length; i++){
+              if(Square.squares[i].nextX == target.nextX &&
+                  Square.squares[i].nextY == target.nextY && Square.squares[i].id != target.id){
+                target = Square.squares[i];
+                break;
+              }
+            }
+
+            target.nextY += 1;
+            if(!isGoingRight && target.nextX == 0)
+              isGoingRight = true;
+          break;
+
+          case 1:
+            var target = null;
+
+            for(var i = 0; i < Square.squares.length; i++)
+              if(Square.squares[i].nextY == 2)
+                target = Square.squares[i];
+
+            target.nextX += isGoingRight ? 1 : -1;
+          break;
+
+          case 2:
+            var target = null;
+
+            for(var i = 0; i < Square.squares.length; i++)
+              if(Square.squares[i].nextY == 2)
+                target = Square.squares[i];
+
+            target.nextY -= 1;
+
+            for(var i = 0; i < Square.squares.length; i++){
+              if(Square.squares[i].nextX == target.nextX &&
+                  Square.squares[i].nextY == target.nextY && Square.squares[i].id != target.id){
+                target = Square.squares[i];
+                break;
+              }
+            }
+
+            if(target.nextX == 3)
+              isGoingRight = false;
+            target.nextY -= 1;
+          break;
+
+          case 3:
+            var target = null;
+
+            for(var i = 0; i < Square.squares.length; i++)
+              if(Square.squares[i].nextY == 0)
+                target = Square.squares[i];
+
+            target.nextX += isGoingRight ? 1 : -1;
+          break;
+        }
+        step = (step + 1) % 4;
+      }, 250);
+    });
+// window.onload = funcion(){
+  
+// };
+
 $(document).ready(function(){
       $(".navbar-mobile__btn-close, .mask, .menu-toggle, .btn-outline-ligther, .navbar-mobile__nav__item__link").click(function(){
         $(".navbar-mobile").toggleClass("shown");
@@ -190,6 +343,7 @@ var confs = [];
 for(var i = 0; i < confNum; i++)
   confs.push(new Confetti());
 window.onload = function() {
+  $("#loader").fadeOut(200);
   requestAnimationFrame(loop);
 };
 
